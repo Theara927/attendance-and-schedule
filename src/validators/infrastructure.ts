758e46c0623520aa8f6dc71be6cf9ffb49444b05
Theara927 <thearasith8181@gmail.com>
2026-03-25 15:33:33 +0700

    feat: Add Zod schemas and types for building and classroom entities.

--- src/validators/infrastructure.ts
+++ src/validators/infrastructure.ts
@@ -1,19 +1,19 @@
-import z, { boolean, string } from "zod";
+import z from "zod";
 
 /**
  * Building Schemas
  */
 
 export const buildingSchema = z.object({
-  name: string().min(3).max(99),
-  isActive: boolean().optional(),
+  name: z.string().min(3).max(99),
+  isActive: z.boolean().optional(),
 });
 export const buildingUpdateSchema = buildingSchema.partial();
 export const buildingQuerySchema = z.object({
-  name: string().optional(),
-  isActive: boolean().optional(),
-  page: z.number().int().positive().optional(),
-  limit: z.number().int().positive().optional(),
+  name: z.string().optional(),
+  isActive: z.coerce.boolean().optional(),
+  page: z.coerce.number().int().positive().optional(),
+  limit: z.coerce.number().int().positive().optional(),
 });
 
 export type BuildingInput = z.infer<typeof buildingSchema>;
@@ -25,18 +25,18 @@ export type BuildingQueryInput = z.infer<typeof buildingQuerySchema>;
  */
 export const classroomSchema = z.object({
   classroomNumber: z.number().int().positive(),
-  name: string().min(3).max(99),
+  name: z.string().min(3).max(99),
   buildingId: z.number().int().positive(),
   floor: z.number().int().positive(),
-  isAvailable: boolean().optional(),
+  isAvailable: z.boolean().optional(),
 });
 export const classroomUpdateSchema = classroomSchema.partial();
 export const classroomQuerySchema = z.object({
-  name: string().optional(),
-  floor: z.number().int().positive().optional(),
-  isAvailable: boolean().optional(),
-  page: z.number().int().positive().optional(),
-  limit: z.number().int().positive().optional(),
+  name: z.string().optional(),
+  floor: z.coerce.number().int().positive().optional(),
+  isAvailable: z.coerce.boolean().optional(),
+  page: z.coerce.number().int().positive().optional(),
+  limit: z.coerce.number().int().positive().optional(),
 });
 
 export type ClassroomInput = z.infer<typeof classroomSchema>;
